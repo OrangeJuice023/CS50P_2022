3d590fb4a7ac88ec0f64c4e400b7d2fc7e4bf60c
Gervi Paulo C. Corado <87125976+OrangeJuice023@users.noreply.github.com>
2023-08-06 03:19:11 +0800

    Create einstein.py

---
+++ Week 0/einstein.py
@@ -0,0 +1,10 @@
+# get the input from the user and convert it into an integer
+mass_object = int(input("mass_object: "))
+
+# adds in the constant of speed of light
+speed_of_light = 300000000
+
+Joules = mass_object * (speed_of_light ** 2)
+
+# prints final output
+print(Joules)
