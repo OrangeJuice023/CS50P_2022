afbdf76b6780b98d9887a0003b003c65ff371cae
Gervi Paulo C. Corado <87125976+OrangeJuice023@users.noreply.github.com>
2023-08-06 03:28:24 +0800

    Create nutrition.py

---
+++ Week 2/nutrition.py
@@ -0,0 +1,25 @@
+# Make a dictionary of the keyvalue pair of fruits with their corresponding calories
+cf = {
+  "apple": 130, "avocado": 50,
+  "banana":110, "cantaloupe": 50,
+  "grapefruit": 60, "grapes": 90,
+  "honeydew melon": 50, "kiwifruit": 90,
+  "lemon": 15, "lime": 20,
+  "nectarine": 60, "orange": 80,
+  "peach": 60, "pear": 100,
+  "pineapple": 50, "plums": 70,
+  "strawberries": 50, "sweet cherries": 100,
+  "tangerine": 50, "watermelon": 80,
+}
+
+# Gets the input of the user
+from_user = input("Item: ").lower()
+
+# Loops through the dictionary
+for checker in cf:
+
+  # Finds the designated fruit
+  if checker == from_user:
+
+    # Print the calories of the fruit
+    print(f"Calories: {cf[checker]}")
