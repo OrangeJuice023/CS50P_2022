32f7741f25961ef21a910cb248d1f730752e60a5
Gervi Paulo C. Corado <87125976+OrangeJuice023@users.noreply.github.com>
2023-08-06 03:18:09 +0800

    Create playback.py

---
+++ Week 0/playback.py
@@ -0,0 +1,5 @@
+#gets user input then automatically replaces the whitespaces with three dots
+user_input = input("Dear user, type here your message:").replace(" ", "...")
+
+#prints the message with replaced spaces
+print(user_input)
